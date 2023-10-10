8af801d10290148c6eae0e1c2cd6fed1f122fe9a
KEVIN VÁZQUEZ <kevin.vazquez@epia.granollers>
2023-10-10 20:36:40 +0200

    Cliente ok

--- ClientesServers/Program.cs
+++ ClientesServers/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace SegundaAct2
 {
@@ -9,53 +10,30 @@ namespace SegundaAct2
         static void Main(string[] args)
         {
             //Creamos servidor
-            TcpListener listener = new TcpListener(80);
-            listener.Start(4); //Cola
+            //TcpListener listener = new TcpListener(4534);
+            //listener.Start(4); //Cola
 
             bool end = false;
-            TcpClient client1 = null;
-            TcpClient client2 = null;
-            TcpClient client3 = null;
-            TcpClient client4 = null;
-            
+
+            TcpClient client1 = new TcpClient();
+            client1.Connect("127.0.0.1", 64321);
+
+            NetworkStream ns1 = client1.GetStream();
+
+            StreamWriter sw = new StreamWriter(ns1);
+            StreamReader sr = new StreamReader(ns1);
+
             while (end == false)
             {
-                if (client1 == null || client1.Connected == false)
-                { 
-                     client1 = listener.AcceptTcpClient();
-                }
-                if (listener.Pending() == true && (client2 == null || client2.Connected == false))
-                {
-                    client2 = listener.AcceptTcpClient();
-                }
-                if (listener.Pending() == true && (client3 == null || client3.Connected == false))
-                {
-                    client3 = listener.AcceptTcpClient();
-                }
-                if (listener.Pending() == true && (client4 == null || client4.Connected == false))
-                {
-                    client4 = listener.AcceptTcpClient();
-                }
+                string msg1 = sr.ReadLine();
+                Console.WriteLine(msg1);
 
-                //En casos urgentes
-                client1.NoDelay = true;
+                string msg = Console.ReadLine();
+                sw.WriteLine(msg);
 
-                NetworkStream ns1 = client1.GetStream();
-                NetworkStream ns2 = client2.GetStream();
-                NetworkStream ns3 = client3.GetStream();
-                NetworkStream ns4 = client4.GetStream();
-
-                //Game loop
-
-                if (end == true) {
-                    client1.Close();
-                    client2.Close();
-                    client3.Close();
-                    client4.Close();
-                }
+                sw.Flush();
             }
 
-            listener.Stop();
 
         }
     }
